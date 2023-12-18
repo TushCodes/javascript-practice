a8b0d4e5e76443b9b8e909e808d2d38d41add54f
TushCodes <42437750+TushCodes@users.noreply.github.com>
2023-12-18 14:32:49 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 144553
+let accountEmail = "tushsachdeva4@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState;
+
+// accountID = 2
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Kurukshetra"
+
+console.log(accountID);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treating all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+// console.log(3+3); console.log("Tushar") // not good readability. code readability should be high
+
+let name="Tushar"
+let age = 24
+let isLoggedIn = false
+
+// number  => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)

31a365534457db5e83b0fc890d1e618ee5ee70de
TushCodes <42437750+TushCodes@users.noreply.github.com>
2023-12-18 20:58:10 +0000

    numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance.toString())
+console.log(balance.toFixed(1))
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// max and min-value
+let max = Number.MAX_VALUE
+let min = Number.MIN_VALUE
